3de35868a0f5ed534c6798bf1adddda0ab0a7aa5
Galib Hassan <galib.hassan.0@gmail.com>
2020-12-10 14:04:16 +0100

    Generate sin(t) graph and freq-deg plots

--- test.py
+++ test.py
@@ -5,23 +5,28 @@ import visibilityGraph as vg
 
 
 GRAPH_DIR_PATH = 'generatedGraphs'
-PLOT_DIR_PATH = GRAPH_DIR_PATH
+PLOT_DIR_PATH = 'plots'
 
-t = np.arange(0, 3*np.pi, np.pi/10)
-ts = np.sin(t)
-fileName = 'sin(t)__0-3pi__0.1pi'
-visGraph = vg.getVisibilityGraph(t, ts, options={"visualize": False})
-nx.write_adjlist(visGraph, f'{GRAPH_DIR_PATH}/{fileName}.adjlist')
+indices = np.arange(3, 300, 2)
+interval = 0.1
+for i in indices:
+    t = np.arange(0, i*np.pi, interval*np.pi)
+    ts = np.sin(t)
+    fileName = f'sin(t)__0-{i}pi__{interval}pi'
+    visGraph = vg.getVisibilityGraph(t, ts, options={"visualize": False})
+    nx.write_adjlist(visGraph, f'{GRAPH_DIR_PATH}/{fileName}.adjlist')
 
-degreeFreq = nx.degree_histogram(visGraph)
-degreesPossible = range(len(degreeFreq))
-plt.bar(degreesPossible, degreeFreq, zorder=0, width=0.1)
-plt.scatter(degreesPossible, degreeFreq, zorder=1)
-plt.xlabel('Degree')
-plt.ylabel('Frequency')
+    degreeFreq = nx.degree_histogram(visGraph)
+    degreesPossible = range(len(degreeFreq))
+    # plt.bar(degreesPossible, degreeFreq, zorder=0, width=0.1)
+    plt.plot(degreesPossible, degreeFreq, 'o-', zorder=1)
+    plt.xlabel('Degree')
+    plt.ylabel('Frequency')
+    plt.title(
+        f"Degree frequency of f(t)=sin(t) visibility graph \nin [0, {indices[0]} to {i}*pi] with {interval} t-increment \n#Node = {visGraph.number_of_nodes()}, #Edges = {visGraph.number_of_edges()}")
 
-imageFileName = f'{PLOT_DIR_PATH}/{fileName}.png'
-plt.savefig(imageFileName, dpi=None, facecolor='w', edgecolor='w',
+    imageFileName = f'{PLOT_DIR_PATH}/{fileName}.png'
+    plt.savefig(imageFileName, dpi=None, facecolor='w', edgecolor='w',
                 orientation='portrait', papertype=None, format=None,
                 transparent=False, bbox_inches=None, pad_inches=0.1,
                 metadata=None)
