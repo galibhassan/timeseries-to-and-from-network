2f59797bcb13753d6fe3d4a1bb03fed8e67f1c71
Galib Hassan <galib.hassan.0@gmail.com>
2020-12-10 01:59:31 +0100

    Find and visualize allowed and unallowed edges

--- visibilityGraph.py
+++ visibilityGraph.py
@@ -10,37 +10,38 @@ def isVisible(ta, ya, tb, yb, tc, yc):
     else:
         return False
 
-def getVisibilityGraph(ts, plt):
+def getVisibilityGraph(t, ts, plt):
     for i in range(len(ts)):
         for j in range(i+1, len(ts)):
             tsInBetween = ts[i+1:j]
             if len(tsInBetween) == 0:
                 # i, j consecutive indices; so visible
                 # print(f"{ts[i]} is visible from {ts[j]}")
-                plt.plot([i, j], [ts[i],ts[j]], color="green")
-                pass
+                plt.plot([t[i], t[j]], [ts[i],ts[j]], color="green", zorder=200)
             elif len(tsInBetween) > 0:
                 stillVisible = True
                 for k in range(len(tsInBetween)):
-                    visFlag = isVisible(
-                        i, ts[i], j, ts[j], (i+1+k), tsInBetween[k])
+                    visFlag = isVisible(t[i], ts[i], t[j], ts[j], t[i+1+k], tsInBetween[k])
                     if visFlag == False:
                         # print(f"{i},{ts[i]} not visible from {j},{ts[j]} because of obstacle {i+1+k},{tsInBetween[k]}")
-                        plt.plot([i, j], [ts[i],ts[j]], color="lightgray")
+                        plt.plot([t[i], t[j]], [ts[i],ts[j]], color="lightgray", zorder=199)
                         stillVisible = False
                         break
 
                 if stillVisible == True:
                     # print(f"{ts[i]} is visible from {ts[j]}")
-                    plt.plot([i, j], [ts[i],ts[j]], color="green")
+                    plt.plot([t[i], t[j]], [ts[i],ts[j]], color="green", zorder=200)
 
 # test
 # t = [0, 1, 2, 3, 4, 5]
 # ts = [0, 1, 20, 3, 4, 100]
-t = np.arange(0, 10, 0.2)
-ts = np.sin(t)
+t = np.arange(0, 7*np.pi, np.pi/10)
+ts = np.sin(t) +1
 plt.bar(t, ts, width=.05, zorder=99)
-plt.scatter(t, ts, zorder=100)
-getVisibilityGraph(ts, plt)
+plt.scatter(t, ts, zorder=300)
+getVisibilityGraph(t, ts, plt)
+plt.title("Visibility graph of $f(t)= sin(t)+1$")
+plt.text(0, -0.2, 'Green lines are edges \nGray lines are failed edges', style='italic',
+        bbox={'facecolor': 'white', 'alpha': 1, 'pad': 5}, zorder=1000)
 
 plt.show()
